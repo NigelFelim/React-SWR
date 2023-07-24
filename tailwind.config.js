82d6cadfad8b9d16f9ef83ffe5235cfa7b253389
NigelFelim <49188239+NigelFelim@users.noreply.github.com>
2023-07-24 16:24:21 +0700

    WIP: Add background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        todoBg: "url(assets/todo.jpg)",
+        postBg: "url(assets/post.jpg)"
+      },
+    },
   },
   plugins: [],
 }
