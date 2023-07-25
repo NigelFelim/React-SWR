eb47a798122540adcdef26d8e5f0fec0b1ba349b
NigelFelim <49188239+NigelFelim@users.noreply.github.com>
2023-07-25 08:30:17 +0700

    Style: Add padding x and text align center

--- src/features/dashboard/components/DashboardPage.tsx
+++ src/features/dashboard/components/DashboardPage.tsx
@@ -11,12 +11,12 @@ const DashboardPage: React.FC = () => {
             <div className="flex items-center justify-between gap-10">
                 <div onClick={() => navigate("/posts")} className="w-full h-[350px] bg-postBg bg-cover border border-solid shadow-2xl cursor-pointer rounded-md">
                     <div className="flex items-center justify-center h-full bg-black bg-opacity-70">
-                        <p className="text-white font-bold text-4xl">Post List Page</p>
+                        <p className="text-white font-bold text-4xl px-3 text-center">Post List Page</p>
                     </div>
                 </div>
                 <div onClick={() => navigate("/todos")} className="w-full h-[350px] bg-todoBg bg-cover border border-solid shadow-2xl cursor-pointer rounded-md">
                     <div className="flex items-center justify-center h-full bg-black bg-opacity-70">
-                        <p className="text-white font-bold text-4xl">To Do List Page</p>
+                        <p className="text-white font-bold text-4xl px-3 text-center">To Do List Page</p>
                     </div>
                 </div>
             </div>
