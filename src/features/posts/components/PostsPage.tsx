8e7e80750fcbd7b8211495fd51021898d282cb69
NigelFelim <49188239+NigelFelim@users.noreply.github.com>
2023-07-25 09:26:46 +0700

    Feat: Change var name and implement Add Post (Normal)

--- src/features/posts/components/PostsPage.tsx
+++ src/features/posts/components/PostsPage.tsx
@@ -4,16 +4,27 @@ import useGetPostsList from "../services/useGetPostsList";
 import PostCard from "./PostCard";
 import CreateOrUpdatePostModel from "../../../model/posts/CreateOrUpdatePostModel";
 import PostForm from "./PostForm";
+import useAddPost from "../services/useAddPost";
+import Alert from "../../../components/Alert";
 
 const PostsPage: React.FC = () => {
+    const { addNewPost } = useAddPost();
+
     const [postsData, setPostsData] = useState<GetPostsListModelData[]>([]);
-    const [open, setOpen] = useState<boolean>(false);
+    const [openDialog, setOpenDialog] = useState<boolean>(false);
+    const [openAlert, setOpenAlert] = useState<boolean>(false);
 
     const { data } = useGetPostsList();
 
-    const onSubmit = (formData: CreateOrUpdatePostModel) => {
-        console.log(formData);
-        setOpen(false);
+    const onSubmit = async (formData: CreateOrUpdatePostModel) => {
+        try {
+            addNewPost(formData)
+        } catch (error) {
+            setOpenDialog(false);
+            setOpenAlert(true);
+        }
+
+        setOpenDialog(false);
     }
 
     useEffect(() => {
@@ -25,7 +36,7 @@ const PostsPage: React.FC = () => {
         <div className="p-10">
             <p className="text-4xl font-bold mb-9">Posts Page</p>
             <div className="flex flex-row-reverse w-full mb-7">
-                <button type="button" className="bg-blue-800 text-white" onClick={() => setOpen(true)}>Add New To Do</button>
+                <button type="button" className="bg-blue-800 text-white" onClick={() => setOpenDialog(true)}>Add New To Do</button>
             </div>
             {
                 postsData.length > 0 && postsData.map((item, index) => 
@@ -34,7 +45,9 @@ const PostsPage: React.FC = () => {
             }
         </div>
 
-        <PostForm dialogTitle="Tambah Post Baru" open={open} onClose={() => setOpen(false)} onSubmit={onSubmit} />
+        <PostForm dialogTitle="Tambah Post Baru" open={openDialog} onClose={() => setOpenDialog(false)} onSubmit={onSubmit} />
+
+        <Alert open={openAlert} handleClose={() => setOpenAlert(false)} title="Gagal" content="Gagal Menambahkan Data" />
         </>
     );
 }
--- src/features/todos/components/TodosPage.tsx
+++ src/features/todos/components/TodosPage.tsx
@@ -4,16 +4,18 @@ import useGetTodosList from "../services/useGetTodosList";
 import TodoCard from "./TodoCard";
 import TodoForm from "./TodoForm";
 import CreateOrUpdateTodoModel from "../../../model/todos/CreateOrUpdateTodoModel";
+import Alert from "../../../components/Alert";
 
 const TodosPage: React.FC = () => {
     const [todosData, setTodosData] = useState<GetTodosListModelData[]>([]);
-    const [open, setOpen] = useState<boolean>(false);
+    const [openDialog, setOpenDialog] = useState<boolean>(false);
+    const [openAlert, setOpenAlert] = useState<boolean>(false);
 
     const { data } = useGetTodosList();
 
     const onSubmit = (formData: CreateOrUpdateTodoModel) => {
         console.log(formData);
-        setOpen(false);
+        setOpenDialog(false);
     }
 
     useEffect(() => {
@@ -25,7 +27,7 @@ const TodosPage: React.FC = () => {
         <div className="p-10">
             <p className="text-4xl font-bold mb-9">To Do Page</p>
             <div className="flex flex-row-reverse w-full mb-7">
-                <button className="bg-blue-800 text-white" onClick={() => setOpen(true)}>Add New To Do</button>
+                <button className="bg-blue-800 text-white" onClick={() => setOpenDialog(true)}>Add New To Do</button>
             </div>
             {
                 todosData.length > 0 && todosData.map((item, index) => 
@@ -34,7 +36,9 @@ const TodosPage: React.FC = () => {
             }
         </div>
 
-        <TodoForm dialogTitle="Tambah To Do Baru" open={open} onClose={() => setOpen(false)} onSubmit={onSubmit} />
+        <TodoForm dialogTitle="Tambah To Do Baru" open={openDialog} onClose={() => setOpenDialog(false)} onSubmit={onSubmit} />
+
+        <Alert open={openAlert} handleClose={() => setOpenAlert(false)} title="Gagal" content="Gagal Menambahkan Data" />
         </>
     );
 }
