8361cbe8632e967f0a709e49ec3f6aeee1e28446
NigelFelim <49188239+NigelFelim@users.noreply.github.com>
2023-07-25 12:35:41 +0700

    Style: Set Left to 0

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -9,7 +9,7 @@ interface Props {
 const Dialog: React.FC<Props> = (props) => {
     return (
         props.open ?
-        <div className="absolute top-0 bg-black bg-opacity-50 w-screen h-screen z-[100] flex items-center justify-center">
+        <div className="absolute top-0 left-0 bg-black bg-opacity-50 w-screen h-screen z-[100] flex items-center justify-center">
             <div className="bg-white w-[500px] max-h-[80vh] p-5">
                 <p className="font-bold text-2xl mb-8">{props.dialogTitle}</p>
                 <div className="mb-2">
