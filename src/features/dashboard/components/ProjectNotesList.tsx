f75fb1061e4be35e53c0537dab2f995fce407e61
NigelFelim <49188239+NigelFelim@users.noreply.github.com>
2023-07-27 11:21:34 +0700

    Chore: Add note

--- src/features/dashboard/components/ProjectNotesList.tsx
+++ src/features/dashboard/components/ProjectNotesList.tsx
@@ -6,7 +6,8 @@ const ProjectNotesList: React.FC = () => {
         "This project was created to test SWR (Stale-While-Revalidate) to help achieve an optimistic UI rendering",
         "Every data on this project is saved locally on 'db.json' file and the initial data is obtained from 'https://jsonplaceholder.typicode.com'",
         "If you want to use a local database ('db.json' file), you can use 'json-server --watch db.json' command on new terminal",
-        "Please note that you might find some style issues"
+        "Please note that you might find some style issues",
+        "If you check the code, you'll probably find comments in some parts. Please DO NOT delete it because is used for notes and explanations"
     ];
 
     return (
